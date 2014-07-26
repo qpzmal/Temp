18f5518bf32f10d378d07422b54d83f66dfe4a88
qpzmal <qpzmal@gmail.com>
2014-07-26 12:33:14 +0800

    时间久了，忘了加了些什么

--- src/threadTest/Test.java
+++ src/threadTest/Test.java
@@ -1,24 +1,71 @@
 package threadTest;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Test {
-	public Test () {
-		a = "b";
-	}
-	String a = "bb";
-	public static void main(String[] args) throws IOException {
-		Test t = new Test();
-		System.out.println(t.a);
-	}
-	
+    private static Queue<String> queue = new ConcurrentLinkedQueue<String>();
+    private static final int threadNum = 3;
+
+    public static void main(String[] args) throws Exception {
+        insertThread it = new insertThread();
+        it.start();
+        ConcumerThread pt = new ConcumerThread();
+        for (int i = 0; i < threadNum; i++) {
+            pt = new ConcumerThread();
+            pt.start();
+        }
+    }
+
+    public static boolean insert(String str) throws Exception {
+        System.out.println("insert=" + str + " , queue.size=" + queue.size());
+        queue.offer(str);
+
+        if (queue.size() > 20000) {
+            // do some thing
+        }
+        return true;
+    }
+
+    public synchronized static String get() {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        return queue.poll();
+    }
 }
-abstract class  TestA {}
-class  TestB extends TestA {}
-class  TestC {}
+class ConcumerThread extends Thread {
+ public ConcumerThread() {
+ }
+ public void run() {
+  String str = "";
+  while (true) {
+   str = Test.get();
+   if(str == null){
+    System.out.println("任务池为空！");
+    try {
+     Thread.sleep(5*1000L);
+    } catch (InterruptedException e) {
+     e.printStackTrace();
+    }
+   }else{
+    System.out.println(Thread.currentThread().getName()+"="+str);
+   }
+    
+  }
+ }
+}
+class insertThread extends Thread {
+ public insertThread() {
+ }
+ public void run() {
+  int i = 0;
+  while (true) {
+    try {
+     Test.insert("insert" + (i++));
+    } catch (Exception e) {
+     e.printStackTrace();
+    }
+  }
+ }
+}
