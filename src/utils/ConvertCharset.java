0515d4115665fbc16fd8d555da846953a4ec5cc4
qpzmal <qpzmal@gmail.com>
2015-01-07 17:09:30 +0800

    工具类：文本编码格式转换

---
+++ src/utils/ConvertCharset.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * 想做一个文本编码格式转换类。
+ * 需求：输入参数：
+ *      1.输入文件夹路径(eg: c:/asdf/test/in/)
+ *      2.目标文件类型(eg: java)
+ *      3.当前格式(eg: utf-8)
+ *      4.目标格式(eg: gb2312)
+ *      5.输出文件夹路径(eg: c:/asdf/test/out/)
+ *      根据以上参数，将参数1下的所有目标文件转换格式后输出到参数5的路径下
+ * @author weiqz
+ *
+ */
+public class ConvertCharset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
